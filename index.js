607e34a0d1a28db4a5873337337086f07c4fe8c1
Joel Lehtonen <jlehtone@c1r5p14.hive.fi>
2024-10-14 12:57:07 +0300

    added get user

--- index.js
+++ index.js
@@ -34,18 +34,35 @@ app.post('/login', async (req, res) => {
 	}
 })
 
-app.get('/user', async (req, res) => {
+// get all users
+app.get('/users', async (req, res) => {
 	try {
 		const result = await pool.query(`
-			SELECT users
+			SELECT users.*
+			GROUP BY users.id;
 			`)
+		res.json(result.rows)
 	}
 	catch (err) {
 		console.error(err);
-		
+		res.status(500).json({error: "Internal server error"})
 	}
 })
 
+// get a specific user
+app.get('/users/:id', async (req, res) => {
+	try {
+		const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+		if (result.rows.length === 0) {
+			return res.status(404).json({error: 'User not found'});
+		}
+		res.json(result.rows[0]);
+	}
+	catch (err) {
+		console.error(err);
+		res.status(500).json({error: "Internal server error"})
+	}
+})
 
 app.use(express.json());
 
