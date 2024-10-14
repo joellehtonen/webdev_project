e9b2697b1cfb2eb12fec02fd0f5a2104ccb8e2e2
Joel Lehtonen <jlehtone@c1r5p14.hive.fi>
2024-10-14 15:29:31 +0300

    work on index.js

--- backend/index.js
+++ backend/index.js
@@ -74,7 +74,54 @@ app.delete('/users/:id', async (req, res) => {
 	}
 })
 
-app.use(express.json());
+// get all likes from an user
+app.get('/users/:usersId/likes', async (req, res) => {
+	const {userId} = req.params;
+	try {
+		const result = await pool.query(`
+			SELECT likes.* FROM likes
+			WHERE likes.user_id = $1`, [userId]
+		)
+		if (result.rows.length === 0) {
+			return res.status(404).json({error: 'User not found'});
+		}
+		res.json(result.rows);
+	}
+	catch (err) {
+		console.error(err);
+		res.status(500).json({error: "Internal server error"})
+	}
+})
+
+// add a like to an user
+app.post('/likes/userId:', async (req, res) => {
+	const {userId} = req.params;
+	const {pokemonId} = req.body;
+	if (!pokemonId)
+			return res.status(400).json({error: "pokemonId required"});
+	try {
+		await pool.query(`
+			INSERT INTO likes (userId, pokemonId) VALUES ($1, $2)`, [userId, pokemonId]);
+	res.status(201).json({message: "Pokemon liked"});
+	}
+	catch (err) {
+		console.error(err);
+		res.status(500).json({error: "Internal server error"})
+	}
+})
+
+// delete a like from an user
+app.delete('/likes/userId:', async (req, res) => {
+	try {
+		const result = await pool.query (`
+			DELETE FROM likes WHERE likes_id == $1`, [likesId])
+		res.json(({message: "Like removed"}))
+		}
+		catch (err) {
+			console.error(err);
+			res.status(500).json({error: "Internal server error"})
+		}
+})
 
 app.listen(port, () => {
 	console.log('Server running on http://localhost:${port}')
