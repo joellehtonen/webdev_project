57c7d2b37944affa74e2ae73062f4e697dee7054
Peter Leander <pleander@student.hive.fi>
2024-10-21 10:16:59 +0300

    Add feature to get username by auth token

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,23 @@ app.get('/users/:id', async (req, res) => {
 	}
 })
 
+// Get username by authtoken
+app.post('/get_user_by_token', authMiddleware, async (req, res) => {
+	console.log(req.user.id);
+	const userId = req.user.id;
+	try {
+		const result = await pool.query('SELECT id, username FROM users where id = $1', [userId]);
+		if (result.rows.length === 0) {
+			return res.status(404).json({error: 'User not found'});
+		}
+		res.json(result.rows[0]);
+	}
+	catch (err) {
+		console.error(err);
+		res.status(500).json({error: "Internal server error"})
+	}
+});
+
 app.listen(port, () => {
 	console.log(`Server running on http://localhost:${port}`)
 })
